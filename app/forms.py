789b550e131987513fff1858272e223ac8c71b2b
Sophie <smf115@ic.ac.uk>
2020-04-29 15:29:32 +0100

    Style/nicer template working from tutorial

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@
 # Contains moels to determine the data being captured by the forms
 # and the logic whether they have been filled in correctly
 
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email 
 
@@ -10,7 +10,10 @@ from wtforms.validators import DataRequired, Email
 # Create python class for the offer (volunteer) form
 class OfferForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[Email(message=('Not a valid email address.')), DataRequired()])
+    email = StringField('Contact email', validators=[Email(message=('Not a valid email address.')), DataRequired()])
+    country = StringField('Country of Location')
+    # helptype = 
+    # recaptcha = RecaptchaField()
     submit = SubmitField('Submit')
 
 # Create python class for the call (caregiver) form
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 """ Routes for what a user should be served when visiting the url within the app"""
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for
 from flask import current_app as app
 
 from .forms import OfferForm
@@ -9,9 +9,7 @@ from .forms import OfferForm
 @app.route('/')
 def home():
     title = 'kotikoulu'
-    description = 'Helping with homeschooling'
-    return render_template('home.html', title=title, description=description)
-
+    return render_template('home.html', title=title)
 
 # Route: offer/volunteer form
 @app.route('/offerform', methods=('GET', 'POST'))
