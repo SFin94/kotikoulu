8f8675bdfba78b3f24660b5946badbb0bbb21905
Sophie <smf115@ic.ac.uk>
2020-05-05 23:23:57 +0100

    Added initial production and testing classes

--- config.py
+++ config.py
@@ -17,3 +17,16 @@ class Config:
     # Database Config
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/kkouludb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # RECAPTCHA settings
+    RECAPTCHA_PUBLIC_KEY = '6LdikPIUAAAAADhsjtdChwyQVAz59xPZEukPgUdt'
+    RECAPTCHA_PRIVATE_KEY = '6LdikPIUAAAAAIEbHGVqLC7DvIzKGkiuk8G-WY2G'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
