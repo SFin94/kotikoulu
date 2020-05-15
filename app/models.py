1acb044c3ce04f7851eb012766b62acca9961e3d
Sophie <smf115@ic.ac.uk>
2020-05-15 23:14:17 +0100

    Changes to form template and db inclusion

--- app/models.py
+++ app/models.py
@@ -1,16 +1,36 @@
-# Handles anything to do with the database
-# from flask import current_app as app
+""" Defines database models """
 
-# # base class for model is db.model and stored on the SQL instance created
+from app import db
+from flask import current_app # as app - Not sure on due to next line
 
-# # Declare a Post Model with three attributes 
-# class Post(db.model):
 
-#     id = db.Column(db.Integer(), primary_key=True)
-#     name = db.Column(db.String(80))
-#     # post_text = db.Column(dp.String(255), unique=True)
+class Volunteer(db.Model):
 
-#     def __init__(self, name):
-#         self.name = name
+    __tablename__ = "volunteers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    pronoun = db.Column(db.String(10), nullable=False, unique=False)
+    country = db.Column(db.String(32), nullable=True, unique=False)
+
+    teaching = db.Column(db.Boolean, nullable=False, unique=False)
+    teaching_exp = db.Column(db.Text, nullable=True, unique=False)
+    teaching_help = db.Column(db.Boolean, nullable=True, unique=False)
+
+    help_calls = db.Column(db.Boolean, nullable=False, unique=False)
+    help_resources = db.Column(db.Boolean, nullable=False, unique=False)
+    sc_maths = db.Column(db.Float, nullable=True, unique=False)
+    
+    other_sub = db.Column(db.Text, nullable=True, unique=False)
+    enrichment = db.Column(db.Text, nullable=True, unique=False)
+
+    def __repr__(self):
+        return "{}".format(self.name)
+
+# class Caregiver():
+
+
+
+
         
 
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 """ Routes for what a user should be served when visiting the url within the app"""
 
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, make_response
 from flask import current_app as app
 
 from .forms import OfferForm, TestForm, SubjectsForm
+from .models import db, Volunteer
 
 subject_list = {'math': 'Maths','chem': 'Chemistry','biol': 'Biology', 'phys': 'Physics', 'elit': 'English Literature', 'elan': 'English Language', 'geog': 'Geography', 'hist': 'History','dram': 'Drama','art': 'Art','musi': 'Music','lang': 'Languages (please state below)','othr': 'Other (please state below)'}
 
@@ -13,14 +14,21 @@ def home():
     title = 'kotikoulu'
     return render_template('home.html', title=title)
 
+@app.route('/kiitos')
+def kiitos():
+    return render_template('kiitos.html')
+
+
 # Route: offer/volunteer form
 @app.route('/offerform', methods=('GET', 'POST'))
 def offer():
     SubjectsForm(subject_list)
     form = OfferForm()
     if form.validate_on_submit():
+        form_data = request(form)
         return redirect(url_for('kiitos'))
     return render_template('form_offer.html', form=form, subjects=subject_list, title='Volunteer form')
+
 
 @app.route('/test', methods=('GET', 'POST'))
 def test():
@@ -28,3 +36,14 @@ def test():
     # TestSubjects(subject_list)
     form = TestForm()
     return render_template('test.html', form=form, subjects=subject_list)
+
+
+@app.route('/testdb', methods=['GET'])
+def testdb():
+
+    name = request.args.get('nome')
+    if name:
+        new_volunteer = Volunteer(name=name, email='test@test.com', pronoun='he/him', teaching=False, help_calls=True, help_resources=False, sc_maths=3)
+        db.session.add(new_volunteer)
+        db.session.commit()
+    return make_response(f"{new_volunteer} successfully created!")
